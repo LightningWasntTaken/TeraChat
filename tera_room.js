86f06a2fd4ef09e31a123d2ebf20441e333b2102
LightningWasntTaken <85737041+LightningWasntTaken@users.noreply.github.com>
2022-02-11 14:33:22 +0530

    Add files via upload

--- tera_room.js
+++ tera_room.js
@@ -34,3 +34,25 @@ function addRoom() {
   localStorage.setItem("room_name" , room_name);
   window.location = "tera_chat.html";
 }
+
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+console.log("Room Name : " + Room_names);
+      row = "<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+//End code
+});});}
+getData();
+
+function redirectToRoomName(name) {
+  console.log(name);
+  localStorage.setItem("room_name" , name);
+  window.location = "tera_chat.html";
+}
+
+var user_name_enter_input = localStorage.getItem("user_name");
+
+document.getElementById("heading_three").innerHTML = "Welcome "+user_name_enter_input+"!";
